bf234dcd6c567bc299d4a1986492c1ea767b9443
Ruchika Gupta <32259133+geekruchika@users.noreply.github.com>
2019-06-20 14:29:31 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_unit_test/home.dart';
 
 void main() {
-  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+  testWidgets('Search Text', (WidgetTester tester) async {
     // Test code goes here.
     await tester.pumpWidget(Home());
     final titleFinder = find.text('Email');
